4d05070447ce070cd34b870a8b459d14cd2fada5
Joseph Acosta <ja0310@production.int>
2019-03-06 16:19:56 -0800

    added two AFs

--- 1.0.0.5/vFunctions.psm1
+++ 1.0.0.5/vFunctions.psm1
@@ -2346,3 +2346,119 @@ function UnInstall-VIB
         }
     }
 }
+
+<#
+.SYNOPSIS
+Shows VMs with CD Drives Connected or configured to Start Connected
+.DESCRIPTION
+Returns an object of VM, Name, StartConnected, Connected, AllowGuestControl, IsoPath, HostDevice and RemoteDevice
+for submitted VMs that have a CD Drive Connected or configured to Start Connected.  Non-CD-Connected VMs are skipped.
+.PARAMETER VM
+Output from VMWare PowerCLI Get-VM.  See Examples.
+[VMware.VimAutomation.ViCore.Impl.V1.VM.UniversalVirtualMachineImpl]
+.INPUTS
+VMWare PowerCLI VM from Get-VM:
+[VMware.VimAutomation.ViCore.Impl.V1.VM.UniversalVirtualMachineImpl]
+.OUTPUTS
+[pscustomobject] SupSkiFun.VM.ConnectedCD.Info
+.EXAMPLE
+Retrieve information for one VM:
+Get-VM -Name System01 | Show-ConnectedCD
+.EXAMPLE
+Retrieve information for two VMs, returning object into a variable:
+$myVar = Get-VM -Name System04 , System07 | Show-ConnectedCD
+.EXAMPLE
+Retrieve information for all VMs, returning object into a variable:
+$MyVar = Get-VM -Name * | Show-ConnectedCD
+#>
+
+Function Show-ConnectedCD
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true , ValueFromPipeline = $true)]
+        [VMware.VimAutomation.ViCore.Impl.V1.VM.UniversalVirtualMachineImpl[]]$VM
+    )
+
+    Process
+    {
+        $x = Get-CDDrive -VM $vm
+        foreach ($y in $x)
+        {
+            if ($y.ConnectionState.StartConnected -or $y.ConnectionState.Connected)
+            {
+                $lo = [pscustomobject]@{
+                    VM = $y.Parent.Name
+                    Name = $y.Name
+                    StartConnected = $y.ConnectionState.StartConnected
+                    Connected = $y.ConnectionState.Connected
+                    AllowGuestControl = $y.ConnectionState.AllowGuestControl
+                    IsoPath = $y.isopath
+                    HostDevice = $y.HostDevice
+                    RemoteDevice = $y.RemoteDevice
+                }
+                $lo
+                $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.VM.ConnectedCD.Info')
+            }
+        }
+    }
+}
+
+<#
+.SYNOPSIS
+Provides information on Vsphere Folders
+.DESCRIPTION
+Returns an object of Name, Id, Path, and Type for specified Vsphere Folders.  If multiple folders have the same name,
+they will all be returned with differing Ids and Paths listed.
+.PARAMETER Folder
+Output from VMWare PowerCLI Get-Folder.  See Examples.
+[VMware.VimAutomation.ViCore.Impl.V1.Inventory.FolderImpl]
+.INPUTS
+VMWare PowerCLI Folder from Get-Folder:
+[VMware.VimAutomation.ViCore.Impl.V1.Inventory.FolderImpl]
+.OUTPUTS
+[pscustomobject] SupSkiFun.VSphereFolderInfo
+.EXAMPLE
+Retrieve information for one folder name:
+Get-Folder -Name TEMP | Show-FolderPath
+.EXAMPLE
+Retrieve information for multiple folders, returning object into a variable:
+$myVar = Get-Folder -Name UAT , QA | Show-FolderPath
+.EXAMPLE
+Retrieve information for all folders, returning object into a variable (this may require a few minutes):
+$MyVar = Get-Folder -Name * | Show-FolderPath
+#>
+
+Function Show-FolderPath
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true , ValueFromPipeline = $true)]
+        [VMware.VimAutomation.ViCore.Impl.V1.Inventory.FolderImpl[]]$Folder
+    )
+
+    Process
+    {
+        foreach ($sn in $folder)
+        {
+            $sne = $sn.ExtensionData
+            $fp = $sn.name
+            while ($sne.Parent)
+            {
+                $sne = Get-View $sne.Parent
+                $fp  = Join-Path -Path $sne.name -ChildPath $fp
+            }
+
+            $lo = [PSCustomObject]@{
+                Name = $sn.Name
+                Id = $sn.id
+                Path = $fp
+                Type = $sn.Type
+            }
+            $lo
+            $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.VSphereFolder.Info')
+        }
+    }
+}
