56f87b6b522973ea3ad58b683cae36802cd70a52
Joseph Acosta <ja0310@production.int>
2020-03-11 11:53:21 -0700

    updated af

--- 3.4.5.6/ThreeAF.ps1
+++ 3.4.5.6/ThreeAF.ps1
@@ -1,3 +1,41 @@
+<#
+.SYNOPSIS
+Reconfigures High Availability (HA)
+.DESCRIPTION
+Reconfigures High Availability (HA) for specified VMHosts(s) via task.  No Output by Default.
+.PARAMETER VMHost
+Piped output of Get-VMHost from Vmware.PowerCLI
+.INPUTS
+Results of Get-VMHost from Vmware.PowerCLI
+VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost
+.OUTPUTS
+None
+.EXAMPLE
+Reconfigures High Availability (HA) on one ESX Host:
+Get-VMHost -Name ESX12 | Invoke-HAReconfigure
+.EXAMPLE
+Reconfigures High Availability (HA) on two ESX Hosts:
+Get-VMHost -Name ESX01 , ESX03 | Invoke-HAReconfigure
+#>
+function Invoke-HAReconfigure
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(ValueFromPipeline=$true)]
+        [VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost[]] $VMHost
+    )
+
+    Process
+    {
+        foreach ($vmh in $vmhost)
+        {
+            $vmh.ExtensionData.ReconfigureHostForDAS_Task()
+        }
+    }
+}
+
+
 function Clear-VSphereAlarm
 {
     <#
@@ -14,9 +52,7 @@ function Clear-VSphereAlarm
 
 }
 
-
-
-function Invoke-HAReconfigure
+function Invoke-HAReconfigureSource
 {
     <#
         Get-VMhost | Sort Name | %{$_.ExtensionData.ReconfigureHostForDAS()} DAS_Task alsoâ€¦.
