79d5841f47b847d77e17367253e6427afdf783da
Joseph Acosta <ja0310@production.int>
2020-03-11 17:21:05 -0700

    close

--- 3.4.5.6/ThreeAF.ps1
+++ 3.4.5.6/ThreeAF.ps1
@@ -1,16 +1,39 @@
-
-
-
-
-
+<#
+.SYNOPSIS
+Clears Alarms from Virtual Center
+.DESCRIPTION
+Clears Alarms from Virtual Center.  No output.  A status of Red, Yellow, Gray or Green
+along with an Entity of All, VM or VMHost must be specified.  See Notes.
+.NOTES
+Future Functionality may include the option to submit more than one status color per run.
+The Vsphere API isn't super flexible with clearing alarms.  See Related Links.
+.PARAMETER Status
+Mandatory.  Red, Yellow, Gray, or Green.  Pick one.
+.PARAMETER Entity
+Mandatory.  All, VM, or VMHost.  Pick one.
+.INPUTS
+None
+.OUTPUTS
+None
+.EXAMPLE
+Clear all alarms for all VMs with a red status:
+Clear-VSphereAlarm -Entity VM -Status Red
+.EXAMPLE
+Clear all alarms for all VMHosts with a yellow status:
+Clear-VSphereAlarm -Entity VMHost -Status Yellow
+.EXAMPLE
+Clear all red alarms for all entities:
+Clear-VSphereAlarm -Entity All -Status Red
+.LINK
+https://communities.vmware.com/thread/623890
+#>
 function Clear-VSphereAlarm
 {
-    [CmdletBinding()]
     [CmdletBinding()]
     param
     (
         [Parameter(Mandatory = $true)]
-        [ValidateSet("Red" , "Yellow")]
+        [ValidateSet("Red" , "Yellow", "Gray", "Green")]
         [string] $Status ,
 
         [Parameter(Mandatory = $true)]
@@ -35,25 +58,4 @@ function Clear-VSphereAlarm
         $filt.TypeEntity = $hv.$Entity
         $almg.ClearTriggeredAlarms($filt)
     }
-
-
-
-
-    <#
-    $almg = Get-View AlarmManager
-    $filt = [VMware.Vim.AlarmFilterSpec]::new()
-    $filt.Status += [VMware.Vim.ManagedEntityStatus]::red
-    $filt.TypeEntity = [VMware.Vim.AlarmFilterSpecAlarmTypeByEntity]::entityTypeVm
-    $filt.TypeTrigger = [vmware.vim.AlarmFilterSpecAlarmTypeByTrigger]::triggerTypeEvent
-    $almg.ClearTriggeredAlarms($filt)
-
-    https://communities.vmware.com/thread/623890
-
-    $hv.'All'
-
-    #>
-
 }
-
-
-
